704879c579e0a6857aeb0500814aa1254a0256dc
jazzy <jazzygermosen@gmail.com>
2025-12-08 15:23:51 -0500

    Added the products class and products dao will continue on the next stretch

---
+++ src/main/java/com/pluralsight/NorthWindTradersSpringBoot/ProductDao.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthWindTradersSpringBoot;
+
+public interface ProductDao {
+
+    public static void addProduct(){
+
+    }
+
+    public static void getAll(){
+
+    }
+
+}
--- src/main/java/com/pluralsight/NorthWindTradersSpringBoot/object.java
+++
@@ -1,4 +0,0 @@
-package com.pluralsight.NorthWindTradersSpringBoot;
-
-public class object {
-}
